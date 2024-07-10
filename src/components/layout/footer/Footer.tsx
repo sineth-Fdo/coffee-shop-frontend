1eb4c896a1546e756d2a05e288854aca0df4453c
sineth-Fdo <132679708+sineth-Fdo@users.noreply.github.com>
2024-07-10 18:27:44 +0530

    feat: created login page

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react'
+import Cookies from 'js-cookie'
 
 const Footer = () => {
+
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    setToken(token as string);
+  }, [])
+
   return (
-    <div className='w-full h-[300px] bg-[#000] text-[#fff] flex justify-center items-center'>
-         <h1>Footer is under Development</h1>
-    </div>
+<>
+        {
+      
+          token ? (
+            <div className='w-full h-[300px] bg-[#000] text-[#fff] flex justify-center items-center'>
+              <h1>Footer is under Development</h1>
+            </div>
+          ) : null
+        }
+        
+</>
   )
 }
 
