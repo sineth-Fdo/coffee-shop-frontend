622e9f8cbc1da9ca09684851eeb8dc8a92b96aa1
sineth-Fdo <132679708+sineth-Fdo@users.noreply.github.com>
2024-07-11 23:27:52 +0530

    fix: firebase production deployment

--- src/data/firebase/config.ts
+++ src/data/firebase/config.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  storageBucket: "gs://coffeeshop-580b4.appspot.com",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
