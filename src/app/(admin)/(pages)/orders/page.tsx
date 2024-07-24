f7f1620139a1af352501f042418ac55adab4a247
sineth-Fdo <132679708+sineth-Fdo@users.noreply.github.com>
2024-07-24 19:18:24 +0530

    chore: view order details

--- src/app/(admin)/(pages)/orders/page.tsx
+++ src/app/(admin)/(pages)/orders/page.tsx
@@ -1,22 +1,12 @@
-'use client';
+"use client";
 
-import MainImage from '@/src/components/ui/mainImage';
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
+import { getAllOrdersAPI } from "@/src/api/product/orderAPI";
+import Container from "@/src/components/layout/container";
 import Cookies from "js-cookie";
-
-import React, { useEffect, useState } from 'react'
-import { getAllOrdersAPI } from '@/src/api/product/orderAPI';
+import { useEffect, useState } from "react";
+import TableMain from "./_components/TableMain";
 
 const Page = () => {
-
   const [orders, setOrders] = useState([]);
 
   const getAllOrders = async () => {
@@ -27,63 +17,22 @@ const Page = () => {
     } catch (error) {
       console.log(error);
     }
-  }
-
-  const formatDate = (dateString: string) => {
-    const date = new Date(dateString);
-    return date.toISOString().split('T')[0]; 
-  }
-
-  const formatTime = (dateString: string) => {
-    const date = new Date(dateString);
-    return date.toTimeString().split(' ')[0]; 
-  }
+  };
 
   useEffect(() => {
     getAllOrders();
   }, []);
 
-
-
   return (
     <div>
-        <div className="relative w-full h-[50vh] overflow-hidden bg-[#000]"> 
-            {/* <MainImage alt="main-image" src="/assets/coffee-admin-banner.jpg" /> */}
-        </div>
-
-        <div>
-
-        <Table>
-          <TableCaption> A list of Orders </TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Order Date</TableHead>
-              <TableHead>Order Time</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Customer Name</TableHead>
-              <TableHead>Customer Mobile</TableHead>
-              <TableHead>Total</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {
-              orders.map((order:any) => (
-                <TableRow key={order.id}>
-                  <TableCell className="font-medium">{formatDate(order.date)}</TableCell>
-                  <TableCell className="font-medium">{formatTime(order.date)}</TableCell>
-                  <TableCell>{order.status}</TableCell>
-                  <TableCell>{order.orderedUser.username}</TableCell>
-                  <TableCell>{order.orderedUser.mobile}</TableCell>
-                  <TableCell>LKR {order.orderedUser.total}.000</TableCell>
-                </TableRow> 
-              ))
-            }
-          </TableBody>
-      </Table>
-
-        </div>
+      <div className="relative w-full h-[50vh] overflow-hidden bg-[#000]">
+        {/* <MainImage alt="main-image" src="/assets/coffee-admin-banner.jpg" /> */}
+      </div>
+      <Container>
+        <TableMain orders={orders} />
+      </Container>
     </div>
-  )
-}
+  );
+};
 
 export default Page;
